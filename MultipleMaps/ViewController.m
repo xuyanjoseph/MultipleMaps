935175a5d0a7872227352db9627ddb5b1418c5b6
xuyan <>
2018-08-10 15:10:27 +0800

    Set up base project content

---
+++ MultipleMaps/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MultipleMaps
+//
+//  Created by dev on 2018/8/10.
+//  Copyright © 2018年 dev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
